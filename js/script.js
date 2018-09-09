79384cdc72a2fbb9f65674f3cc1cced47afa6094
Melnikov Maxim <masaraksh2629@gmail.com>
2018-09-09 22:47:21 +1000

    project

--- js/script.js
+++ js/script.js
@@ -27,8 +27,26 @@ window.addEventListener('DOMContentLoaded', () => {
 	};
 
 	call(headerBtn, closeBtn, modal);
-	call(phoneLinkHeader, closeBtnCall, modal);
-	call(phoneLinkFooter, closeBtnCall, modal);
+
+
+	function popupBtn(button, cross, background) {
+
+		button.addEventListener('click', () => {
+			popup.style.display = 'block';
+		});
+
+		cross.addEventListener('click', () => {
+			popup.style.display = 'none';
+		});
+
+		background.addEventListener('click', function(e) {
+			if (e.target == popup) {
+				this.style.display = 'none';
+			}
+		});	
+	};
+	popupBtn(phoneLinkHeader, closeBtnCall, popup);
+	popupBtn(phoneLinkFooter, closeBtnCall, popup);
 
 
     setTimeout("document.querySelector('.popup_engineer').style.display='block'", 60000);
@@ -114,6 +132,71 @@ window.addEventListener('DOMContentLoaded', () => {
 		exclude(inputTelefon[i]);
 	};
 
-//tabs
+//timer
 
+let deadLine = '2018-10-04';
+
+	function getTimeRemaining(endtime) {
+		let t = Date.parse(endtime) - Date.parse(new Date()),
+			seconds = Math.floor((t/1000) % 60).toString(),
+			minutes = Math.floor((t/1000/60) % 60).toString(),
+			hours = Math.floor( (t/(1000*60*60)) % 24 ).toString(),
+			days = Math.floor( t/(1000*60*60*24) ).toString();
+
+			if (days.length < 2) {
+				days = '0' + days;
+			}
+
+			if (hours.length < 2) {
+				hours = '0' + hours;
+			}
+
+			if (minutes.length < 2) {
+				minutes = '0' + minutes;
+			}
+
+			if (seconds.length < 2) {
+				seconds = '0' + seconds;
+			}
+
+			return {
+				'total': t,
+				'days' : days,
+				'hours': hours,
+				'minutes': minutes,
+				'seconds': seconds
+			};
+	};
+
+	function setClock(id, endtime) {
+		let timer = document.getElementById(id),
+			days = timer.querySelector('.days'),
+			hours = timer.querySelector('.hours'),
+			minutes = timer.querySelector('.minuts'),
+			seconds = timer.querySelector('.seconds');
+
+			function updateClock() {
+				let t = getTimeRemaining(endtime)
+					timeInterval  = setInterval(updateClock, 1000);
+
+				days.innerHTML = t.days;
+				hours.innerHTML = t.hours;
+				minutes.innerHTML = t.minutes;
+				seconds.innerHTML = t.seconds;
+
+				if (t.total <= 0) {
+					clearInterval(timeInterval);
+
+					days.innerHTML = '00';
+					hours.innerHTML = '00';
+					minutes.innerHTML = '00';
+					seconds.innerHTML = '00';
+				}
+			};
+
+			updateClock();
+	};
+	setClock('timer', deadLine);
+
+	
 }); 
