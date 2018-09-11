526b71ae065f7315593bedeb13661544dfd953d2
Melnikov Maxim <masaraksh2629@gmail.com>
2018-09-12 03:07:42 +1000

    project

--- js/script.js
+++ js/script.js
@@ -125,78 +125,163 @@ window.addEventListener('DOMContentLoaded', () => {
 	    };
 	};
 
-//telefon number
-	let inputTelefon = document.querySelectorAll('input[name="user_phone"]');
+	//telefon number
+		let inputTelefon = document.querySelectorAll('input[name="user_phone"]');
 
-	for (var i = 0; i < inputTelefon.length; i++) {
-		exclude(inputTelefon[i]);
-	};
+		for (var i = 0; i < inputTelefon.length; i++) {
+			exclude(inputTelefon[i]);
+		};
 
-//timer
+	//timer
 
-let deadLine = '2018-10-04';
+	let deadLine = '2018-10-04';
 
-	function getTimeRemaining(endtime) {
-		let t = Date.parse(endtime) - Date.parse(new Date()),
-			seconds = Math.floor((t/1000) % 60).toString(),
-			minutes = Math.floor((t/1000/60) % 60).toString(),
-			hours = Math.floor( (t/(1000*60*60)) % 24 ).toString(),
-			days = Math.floor( t/(1000*60*60*24) ).toString();
+		function getTimeRemaining(endtime) {
+			let t = Date.parse(endtime) - Date.parse(new Date()),
+				seconds = Math.floor((t/1000) % 60).toString(),
+				minutes = Math.floor((t/1000/60) % 60).toString(),
+				hours = Math.floor((t/(1000*60*60)) % 24).toString(),
+				days = Math.floor(t/(1000*60*60*24)).toString();
 
-			if (days.length < 2) {
-				days = '0' + days;
-			}
-
-			if (hours.length < 2) {
-				hours = '0' + hours;
-			}
-
-			if (minutes.length < 2) {
-				minutes = '0' + minutes;
-			}
-
-			if (seconds.length < 2) {
-				seconds = '0' + seconds;
-			}
-
-			return {
-				'total': t,
-				'days' : days,
-				'hours': hours,
-				'minutes': minutes,
-				'seconds': seconds
-			};
-	};
-
-	function setClock(id, endtime) {
-		let timer = document.getElementById(id),
-			days = timer.querySelector('.days'),
-			hours = timer.querySelector('.hours'),
-			minutes = timer.querySelector('.minuts'),
-			seconds = timer.querySelector('.seconds');
-
-			function updateClock() {
-				let t = getTimeRemaining(endtime)
-					timeInterval  = setInterval(updateClock, 1000);
-
-				days.innerHTML = t.days;
-				hours.innerHTML = t.hours;
-				minutes.innerHTML = t.minutes;
-				seconds.innerHTML = t.seconds;
-
-				if (t.total <= 0) {
-					clearInterval(timeInterval);
-
-					days.innerHTML = '00';
-					hours.innerHTML = '00';
-					minutes.innerHTML = '00';
-					seconds.innerHTML = '00';
+				if (days.length < 2) {
+					days = '0' + days;
 				}
-			};
 
-			updateClock();
+				if (hours.length < 2) {
+					hours = '0' + hours;
+				}
+
+				if (minutes.length < 2) {
+					minutes = '0' + minutes;
+				}
+
+				if (seconds.length < 2) {
+					seconds = '0' + seconds;
+				}
+
+				return {
+					'total': t,
+					'days' : days,
+					'hours': hours,
+					'minutes': minutes,
+					'seconds': seconds
+				};
+		};
+
+		function setClock(id, endtime) {
+			let timer = document.getElementById(id),
+				days = timer.querySelector('.days'),
+				hours = timer.querySelector('.hours'),
+				minutes = timer.querySelector('.minuts'),
+				seconds = timer.querySelector('.seconds');
+
+				function updateClock() {
+					let t = getTimeRemaining(endtime)
+						timeInterval  = setInterval(updateClock, 1000);
+
+					days.innerHTML = t.days;
+					hours.innerHTML = t.hours;
+					minutes.innerHTML = t.minutes;
+					seconds.innerHTML = t.seconds;
+
+					if (t.total <= 0) {
+						clearInterval(timeInterval);
+
+						days.innerHTML = '00';
+						hours.innerHTML = '00';
+						minutes.innerHTML = '00';
+						seconds.innerHTML = '00';
+					}
+				};
+
+				updateClock();
+		};
+		setClock('timer', deadLine);
+
+	//calc
+
+	let calcBtn = document.querySelectorAll('.popup_calc_btn'),
+		calcNextBtn = document.querySelector('.popup_calc_button'),
+		calcProfileBtn = document.querySelector('.popup_calc_profile_button'),
+		popupCalc = document.querySelector('.popup_calc'),
+		widthCalc = document.getElementById('width'),
+		heightCalc = document.getElementById('height'),
+		popupCalcProfile = document.querySelector('.popup_calc_profile'),
+		popupCalcEnd = document.querySelector('.popup_calc_end'),
+		calcClose = document.querySelector('.popup_calc_close'),
+		calcCloseProfile = document.querySelector('.popup_calc_profile_close'),
+		calcCloseEnd = document.querySelector('.popup_calc_end_close'),
+		bigImgPref = document.querySelectorAll('.big_img_pref'),
+		minImgPref = document.querySelectorAll('.min_img_pref')
+
+
+
+
+	//calc button
+	for (var i = 0; i < calcBtn.length; i++) {
+		calcBtn[i].addEventListener('click', function() {
+			popupCalc.style.display = 'block';
+		})
+	}
+
+	//only calc nuber 
+	exclude(widthCalc);
+	exclude(heightCalc);
+
+	//calc button next 
+
+	calcNextBtn.addEventListener('click', function() {
+		popupCalc.style.display = 'none';
+		popupCalcProfile.style.display = 'block';
+	});
+
+	//calc profile btn
+
+	calcProfileBtn.addEventListener('click', function() {
+		popupCalcProfile.style.display = 'none';
+		popupCalcEnd.style.display = 'block';
+	});
+
+	//calc close 
+
+	function closeCalculate(variable, value) {
+		variable.addEventListener('click', function() {
+				value.style.display = 'none';
+			})
 	};
-	setClock('timer', deadLine);
+
+	closeCalculate(calcClose, popupCalc);	
+	closeCalculate(calcCloseProfile, popupCalcProfile);	
+	closeCalculate(calcCloseEnd, popupCalcEnd);
+
+	//miniature
 
 	
+	//tab
+
+	let tab = document.querySelectorAll('.decoration_item'),
+		tabContent = document.querySelectorAll('.decor'),
+		// glazing = document.querySelectorAll('.glazing_block'),
+		// glazingContent = document.querySelectorAll('.glazin'),
+		noClick = document.querySelectorAll('.no_click')
+		// wind = document.querySelectorAll('.wind')
+
+	
+	function hideAllTabs() {
+		for (let i = 0; i < tabContent.length; i++) {
+			tabContent[i].style.display = 'none';
+			noClick[i].classList.remove('after_click');
+		}
+	}
+
+	for (let i = 0; i < tab.length; i++) {
+		tab[i].addEventListener('click', function() {
+			this.classList.add('.active');
+			hideAllTabs();
+			tabContent[i].style.display = 'block';
+			noClick[i].classList.add('after_click');
+		})
+	}
+		
+
 }); 
